025f2b5488f3b32796c89c92f46a5732caff9d49
Erick-Andrade <ericksandrade2@gmail.com>
2026-07-29 14:30:18 -0300

    feat: add Contact entity

---
+++ src/main/java/github/com/Erick_Andrade/contact_manager_api/entity/Contact.java
@@ -0,0 +1,22 @@
+package github.com.Erick_Andrade.contact_manager_api.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Contact {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+}
