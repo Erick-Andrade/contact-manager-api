c2fca6f635daef50fcb8e76ab79437bed5a0ac39
Erick-Andrade <ericksandrade2@gmail.com>
2026-07-29 14:19:26 -0300

    chore: initial project setup

---
+++ src/main/java/github/com/Erick_Andrade/contact_manager_api/ContactManagerApiApplication.java
@@ -0,0 +1,13 @@
+package github.com.Erick_Andrade.contact_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/com/Erick_Andrade/contact_manager_api/ContactManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.Erick_Andrade.contact_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
